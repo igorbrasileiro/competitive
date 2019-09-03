abe0a7994655ce8bad80451af376f08038f3fc97
igorbrasileiro <brasileiro456@gmail.com>
2019-09-02 22:35:06 -0300

    fixing list3/d.cpp

--- aa-intermediary/19.2/list3/d.cpp
+++ aa-intermediary/19.2/list3/d.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 // utility
 void printMatrix(vector< vector<int>> &matrix) {
   for (auto &row: matrix) {
@@ -10,12 +10,12 @@ void printMatrix(vector< vector<int>> &matrix) {
     cout << endl;
   }
 }
-
+ 
 inline int myLog2(int val) {
   return __builtin_clz(1) - __builtin_clz(val);
 }
 // finish utility
-
+ 
 void build_table(vector< vector<int>> &st, int n, vector<int> &input) {
   int k = myLog2(n);
   for (int i = 0; i < n; i++) {
@@ -23,62 +23,51 @@ void build_table(vector< vector<int>> &st, int n, vector<int> &input) {
     row[0] = input[i];
     st[i] = row;
   }
-
+ 
   for (int j = 1; j <= k; j++) {
     for (int i = 0; i + (1 << j) <= n; i++) {
       st[i][j] = min(st[i][j - 1], st[i + (1 << (j - 1))][j - 1]);
     }
   }
 }
-
+ 
 int query(vector< vector<int>> &st, int l, int r) {
   int maxLogPower = myLog2(r - l + 1);
   return min(st[l][maxLogPower], st[r - (1 << maxLogPower) + 1][maxLogPower]);
 }
-
-int left_binary_search(vector< vector<int>> &st, int pos_x) {
-  int l = pos_x / 2;
-  int min_x = query(st, pos_x, pos_x);
-  while (l <= pos_x && l >= 0) {
-    cout << l << endl;
-    if(query(st, l, pos_x) < min_x) {
-      l = (l + pos_x) / 2;
+ 
+int left_binary_search(vector< vector<int>> &st, int l, int r) {
+  int min_x = query(st, r, r);
+  int mid;
+  int res = r;
+  while (l <= r && l >= 0) {
+    mid = (l + r) /2;
+    if(query(st, mid, r) < min_x) {
+      l = mid + 1; // aproxima  
     } else {
-      if (l == 0) {
-        break;
-      }
-      // se a 
-      if (query(st, l - 1, pos_x) == min_x) {
-        l = (l / 2);
-      } else {
-        break;
-      }
+      r = mid - 1; // distancia
+      res = mid;
     }
   }
-  return max(0, l);
+  return res;
 }
-
-int right_binary_search(vector< vector<int>> &st, int pos_x, int max) {
-  int r = (pos_x + max) / 2;
-  int min_x = query(st, pos_x, pos_x);
-  while(r < max && pos_x <= r) {
-    cout << r << endl;
-    if(query(st, pos_x, r) < min_x) {
-      r = (pos_x + r) / 2;
+ 
+int right_binary_search(vector< vector<int>> &st, int l, int r) {
+  int min_x = query(st, l, l);
+  int mid;
+  int res = l;
+  while(l <= r) {
+    mid = (r + l) / 2;
+    if (query(st, l, mid) < min_x) {
+      r = mid - 1; // aproxima
     } else {
-      if (r == max) {
-        break;
-      }
-      if (query(st, pos_x, r + 1) == min_x) {
-        r = (r + max) / 2;
-      } else {
-        break;
-      }
+      l = mid + 1; // distancia
+      res = mid;
     }
   }
-  return r;
+  return res;
 }
-
+ 
 int main() {
   int n;
   scanf("%d", &n);
@@ -86,18 +75,27 @@ int main() {
   for (int i = 0; i < n; i++) {
     scanf("%d", &input[i]);
   }
-
+ 
   vector< vector<int>> st(n);
   build_table(st, n, input);
-
+  vector<int> result(n, 0);
   // reusing input to output
   for (int i = n - 1; i >= 0; i--) {
-    input[i] = 0;
-    cout << i << " left" << endl;
-    int l = left_binary_search(st, i);
-    cout << "right" << endl;
-    int r = right_binary_search(st, i, n);
-    cout << "dsada" << l << " " << r << endl;
+    int l = left_binary_search(st, 0, i);
+    int r = right_binary_search(st, i, n-1);
+    if (result[r - l] < input[i]) {
+      result[r - l] = input[i]; // indexado de 0 então nao soma 1
+    }
   }
+  for (int i = n - 2; i >= 0; i--) {
+    if (result[i] < result[i + 1]) {
+      result[i] = result[i + 1];
+    }
+  }
+ 
+  for (const int c: result) {
+    printf("%d ", c);
+  } printf("\n");
+ 
   return EXIT_SUCCESS;
 }
