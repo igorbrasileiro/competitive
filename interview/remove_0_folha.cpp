9c9b216f1e109d6788e5a1143c3c22c4f77312c7
igorbrasileiro <brasileiro456@gmail.com>
2019-09-04 20:51:47 -0300

    adding emerson mock bt question

---
+++ interview/remove_0_folha.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Tree {
+  int value;
+  Tree *left;
+  Tree *right;
+};
+
+Tree* remove(Tree *tree) {
+  if (tree->value == 0 && tree->left == NULL && tree->right == NULL) {
+    return NULL;
+  }
+
+  Tree *left;
+  Tree *right;
+  if (tree->left != NULL) {
+    left = remove(tree->left);
+    if(left == NULL) tree->left = NULL;
+  }
+  if (tree->right != NULL) {
+    right = remove(tree->right);
+    if(right == NULL) tree->right = NULL;
+  }
+
+  if (left == NULL && right == NULL) {
+    return NULL;
+  } else {
+    return tree;
+  }
+}
+
+int main() {
+   Tree *root = new Tree;
+   root->value = 32;
+   
+   Tree *left = new Tree;
+   left->value = 9;
+   root->left = left;
+
+   Tree *leftright = new Tree;
+   leftright->value = 5;
+   Tree *leftleft = new Tree;
+   leftleft->value = 0;
+   left->left = leftleft;
+   left->right = leftright;
+
+   Tree *right = new Tree;
+   right->value = 0;
+   root->right = right;
+  
+   Tree *rightleft = new Tree;
+   rightleft->value = 0;
+   Tree *rightright = new Tree;
+   rightright->value = 0;
+   right->left = rightleft;
+   right->right = rightright;
+
+
+  root = remove(root);
+  if(root->right == NULL) {
+    printf("TA OK MANO! no direita\n");
+  }
+  if(root->left->left == NULL) {
+    printf("no esquerda\n");
+  }
+
+  return 0;
+}
